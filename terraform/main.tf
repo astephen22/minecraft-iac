87adc5dc0757924755df7d020f3e578b87642144
Alex Stephen <stepalex@oregonstate.edu>
2026-05-31 17:45:14 -0700

    observibility and incident response ops 5

--- terraform/main.tf
+++ terraform/main.tf
@@ -167,7 +167,7 @@ resource "aws_instance" "minecraft_server" {
   ##################################
 
   root_block_device {
-    volume_size = 20
+    volume_size = 30
     volume_type = "gp3"
   }
 
